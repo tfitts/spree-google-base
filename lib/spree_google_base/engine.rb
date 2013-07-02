9183dd628e74b69f603dccf77f895fcab6e5f41a
Randy Terbush <randy@terbush.org>
2013-07-02 17:48:11 -0600

    Add a few more fields to the export and fix a few 2.0 compatibility issues.

--- lib/spree_google_base/engine.rb
+++ lib/spree_google_base/engine.rb
@@ -6,22 +6,24 @@ module SpreeGoogleBase
 
     initializer "spree.google_base.environment", :before => :load_config_initializers do |app|
       Spree::GoogleBase::Config = Spree::GoogleBaseConfiguration.new
-      
+
       # See http://support.google.com/merchants/bin/answer.py?hl=en&answer=188494#US for all other fields
       SpreeGoogleBase::FeedBuilder::GOOGLE_BASE_ATTR_MAP = [
         ['g:id', 'id'],
-        ['g:gtin','upc'],
-        ['g:mpn', 'sku'],
-        ['title', 'name'],
+        ['title', 'google_base_title'],
         ['description', 'google_base_description'],
-        ['g:price', 'price'],
-        ['g:condition', 'google_base_condition'],
+        ['g:google_product_category','google_base_product_category'],
         ['g:product_type', 'google_base_product_type'],
-        ['g:brand', 'google_base_brand'],
-        ['g:quantity','on_hand'],
+        ['g:condition', 'google_base_condition'],
         ['g:availability', 'google_base_availability'],
-        ['g:image_link','google_base_image_link'],
-        ['g:google_product_category','google_product_category']
+        ['g:price', 'google_base_price'],
+        ['g:sale_price', 'google_base_sale_price'],
+        ['g:sale_price_effective_date', 'google_base_sale_price_effective'],
+        ['g:brand', 'google_base_brand'],
+        ['g:gtin','google_base_gtin'],
+        ['g:mpn', 'google_base_mpn'],
+        ['g:shipping_weight', 'google_base_shipping_weight'],
+        ['g:adult', 'google_base_adult']
       ]
     end
 
@@ -32,6 +34,6 @@ module SpreeGoogleBase
     end
 
     config.to_prepare &method(:activate).to_proc
-    
+
   end
 end
